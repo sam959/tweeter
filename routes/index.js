fd303170174c5d3a63029d89406edfa05fde5cca
samantha-lovisolo <samantha.lovisolo@telepass.com>
2022-11-02 19:32:28 +0100

    Update rules, clean

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  req.app.get('io').on('connection', async (socket) => {
-    socket.emit('hello', { message: 'Hello again from server!' })
-  })
-
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tweeter' });
 });
 
 module.exports = router;
--- routes/tweeter.js
+++ routes/tweeter.js
@@ -14,12 +14,8 @@ const  {
 } = require('../controller/mockedTweets')
 
 const  { 
-  setRules, getRules
+  setRules, getRules, deleteRules
 } = require('../controller/rules')
-
-const  { 
-  streamTweets
-} = require('../controller/needleStream')
 
 const {
   initiateStream
@@ -35,45 +31,8 @@ router.get('/', function(req, res, next) {
 });
 */
 
-router.get('/', getRules,getUserInfo, getLocalTweets, initiateStream);
+router.get('/', getRules, deleteRules, setRules, getUserInfo, getTweets, initiateStream);
+//router.get('/', getRules, deleteRules, setRules, getUserInfo, getLocalTweets, initiateStream);
 //router.get('/', getUserInfo, getLocalTweets, getTweets);
-
-
-function localTweets(handle){
-  const baseUrl = `https://api.twitter.com/2/users/${handle}/tweets`;
-
-  console.log(`getting tweets by handle ${handle}`);
-
-  app.get('/', async (req, res) => {
-    if (!process.env.BEARER_TOKEN) {
-      res.status(400).send(authMessage);
-    }
-  
-    const token = BEARER_TOKEN;
-
-    console.log(`sending request tweets by handle ${handle}`);
-
-    const requestConfig = {
-      url: baseUrl,
-      auth: {
-        bearer: token,
-      }
-    };
-  
-    try {
-      console.log(`sending request tweets by handle ${requestConfig}`);
-
-      const response = await post(requestConfig);
-  
-      if (response.statusCode === 200 || response.statusCode === 201) {
-        res.send(response);
-      } else {
-        throw new Error(response);
-      }
-    } catch (e) {
-      res.send(e);
-    }
-  });
-}
 
 module.exports = router;
